1c412daef08148c6642b12bf5b6f5b00b984f3f4
Dnyaneshwar Daphal <dnyaneshwar.daphal@footlocker.com>
2019-11-17 12:53:12 -0600

    Initial Commit - Varun

---
+++ my-app/src/main/java/com/mycompany/app/HelloWorld2.java
@@ -0,0 +1,12 @@
+package com.mycompany.app;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello World2 !");
+	}
+
+}
